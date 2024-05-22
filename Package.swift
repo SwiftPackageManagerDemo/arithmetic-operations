46a196207b5b411de9578429804851499c7fb302
Vivek <iamviveklalan@gmail.com>
2024-05-22 16:49:24 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,19 +1,18 @@
 // swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
 let package = Package(
     name: "Addition",
-    platforms: [
-        .iOS(.v13),
-    ],
     products: [
         .library(
             name: "Addition",
             targets: ["Addition"]),
     ],
     targets: [
-        .binaryTarget(
-            name: "Addition",
-            url: "https://github.com/vivektechyogi/arithmetic-operations/blob/main/Addition/Addition.xcframework.zip",
-            checksum: "7122ff77df419d9553e07d481e0ff499055340914f18abaf2573dd9a431f5035")
-    ]
-)
+      .binaryTarget(
+          name: "Addition",
+         path: "./Addition/Addition.xcframework.zip"),
 
+    ])
