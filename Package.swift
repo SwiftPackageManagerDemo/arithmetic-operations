0206c872061de51c1bb71da307e1e0c57ce03ead
Vivek <iamviveklalan@gmail.com>
2024-05-22 15:50:06 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Addition",
-            url: "https://github.com/vivektechyogi/arithmetic-operations/blob/main/Addition/Addition.xcframework.zip",
-            checksum: "7122ff77df419d9553e07d481e0ff499055340914f18abaf2573dd9a431f5035")
+            path: "Addition/Addition.xcframework"
+        )
     ]
 )
 
