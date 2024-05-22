b0c590fc6262b29849642ba0a705d4474beb8ebe
Vivek <iamviveklalan@gmail.com>
2024-05-22 15:44:25 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,9 @@
 // swift-tools-version: 5.10
 let package = Package(
     name: "Addition",
+    platforms: [
+        .iOS(.v13),
+    ],
     products: [
         .library(
             name: "Addition",
@@ -9,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Addition",
-            url: "https://github.com/vivektechyogi/arithmetic-operations/raw/main/Addition/Addition.xcframework.zip\",
+            url: "https://github.com/vivektechyogi/arithmetic-operations/raw/main/Addition/Addition.xcframework.zip",
             checksum: "7122ff77df419d9553e07d481e0ff499055340914f18abaf2573dd9a431f5035")
     ]
 )
